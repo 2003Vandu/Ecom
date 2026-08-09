a23b679c155c10baffa070e4a7c72060ead79a9c
2003Vandu <patilvandesh301@gmail.com>
2026-08-09 19:43:29 +0530

    Add: Stock Quantity to show Quantity

--- src/main/java/com/eComm/eComm/io/ItemRequest.java
+++ src/main/java/com/eComm/eComm/io/ItemRequest.java
@@ -1,5 +1,6 @@
 package com.eComm.eComm.io;
 
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +19,12 @@ public class ItemRequest
     private String categoryId;
     private String description;
 
+    // ✅ NEW: Stock quantity (optional, defaults to 0)
+    @PositiveOrZero(message = "Stock quantity cannot be negative")//4/19/2026
+    @Builder.Default
+    private Integer stockQuantity = 0;
+
+    @PositiveOrZero(message = "Low stock threshold cannot be negative")//4/19/2026
+    @Builder.Default
+    private Integer lowStockThreshold = 10;
 }
